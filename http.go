21eb8d8daa193840c23948e07c5c12c8cd330bca
Jason Harvey <alienth@gmail.com>
2016-08-11 12:46:21 -0800

    Add some more info.

--- http.go
+++ http.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 	"sort"
 	"time"
 )
@@ -19,7 +20,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if noop {
 		fmt.Fprintf(w, "<h1>In noop mode</h1>\n")
 	}
-	fmt.Fprint(w, "<table><th>IP</th><th>Hits</th><th>Limited</th><th>Hits Per Sec</th>\n")
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Fprintf(w, "<p>Alloc: %d, HeapObjects: %d</p>", m.Alloc, m.HeapObjects)
+	fmt.Fprintf(w, "<p>Total IPs tracked: %d</p>", len(hits.m))
+	fmt.Fprint(w, "<table><th>IP</th><th>Hits</th><th>Limited</th><th>HPS</th><th>Strikes</th><th>Limit Time Remaining</th>\n")
 
 	var rates ipRates
 	for _, ipr := range hitmap {
@@ -30,7 +35,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	for _, ipr := range rates {
 		hps := float64(ipr.Hits) / time.Now().Sub(time.Unix(ipr.FirstHit, 0)).Seconds()
 		if ipr.limited {
-			fmt.Fprintf(w, "<tr><td>%s</td><td>%d</td><td>%t</td><td>%f</td></tr>\n", ipr.ip.String(), ipr.Hits, ipr.limited, hps)
+			fmt.Fprintf(w, "<tr><td>%s</td><td>%d</td><td>%t</td><td>%0.2f</td><td>%d</td><td>%0.2fm</td></tr>\n", ipr.ip.String(), ipr.Hits, ipr.limited, hps, ipr.Strikes, time.Unix(ipr.LimitExpire, 0).Sub(time.Now()).Minutes())
 		}
 	}
 	fmt.Fprint(w, "</table>")
